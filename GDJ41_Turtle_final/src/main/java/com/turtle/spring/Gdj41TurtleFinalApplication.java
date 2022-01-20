71386b808240c15fa46505fecdbebc36d382a931
Kyungmin <sharma93@naver.com>
2022-01-20 11:11:31 +0900

    기본 커밋

---
+++ GDJ41_Turtle_final/src/main/java/com/turtle/spring/Gdj41TurtleFinalApplication.java
@@ -0,0 +1,13 @@
+package com.turtle.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdj41TurtleFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdj41TurtleFinalApplication.class, args);
+	}
+
+}
---
+++ GDJ41_Turtle_final/src/test/java/com/turtle/spring/Gdj41TurtleFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtle.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdj41TurtleFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
