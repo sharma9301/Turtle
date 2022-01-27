102d5c939846056354f902ac864d2639fab61f92
Kyungmin <sharma93@naver.com>
2022-01-27 14:44:24 +0900

    jsp 사용할 수 있도록 모듈 추가후 커밋함 -km-

---
+++ GDJ41_Turtle_final/src/main/java/com/turtle/spring/controller/MvcController.java
@@ -0,0 +1,15 @@
+package com.turtle.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class MvcController {
+	/*
+	 * @RequestMapping("/") public String maping() { return "index1"; }
+	 * 
+	 */
+}
--- GDJ41_Turtle_final/src/main/java/com/turtle/spring/member/controller/MemberController.java
+++ GDJ41_Turtle_final/src/main/java/com/turtle/spring/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.turtle.spring.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.turtle.spring.member.model.service.MemberService;
 
